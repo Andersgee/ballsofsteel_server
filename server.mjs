90f336f410f5a1b8cc09f4870324eb1c87f63c7f
Anders Gustafsson <andersgee@gmail.com>
2020-11-04 23:14:54 +0100

    ok that works

--- server.mjs
+++ server.mjs
@@ -1,73 +1,9 @@
-/*
-import express from "express";
-import socketIO from "socket.io";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const PORT = process.env.PORT || 3000;
-const INDEX = "/public/index.html";
-*/
-/*
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-*/
-/*
-const server = express()
-  .use((req, res) => res.send(`server is up at port ${PORT}`))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-*/
-/*
-const server = express();
-server.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-});
-
-const io = socketIO(server);
-*/
-
-/*
-import http from "http";
-import express from "express";
-import socketIO from "socket.io";
-import { fileURLToPath } from "url";
-import path from "path";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const PORT = process.env.PORT || 3000;
-
-const app = express();
-app.set("port", PORT);
-app.use("/public", express.static(path.join(__dirname, "public")));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-app.get("/port", (req, res) => {
-  res.send(`${PORT}`);
-});
-
-const server = http.Server(app);
-server.listen(PORT, () => {
-  console.log("Starting server.", PORT);
-});
-const io = socketIO(server);
-*/
-
 import express from "express";
 import http from "http";
 import socketIO from "socket.io";
-//import index from "./public/index.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-//app.use(index);
 app.get("/port", (req, res) => {
   res.send(`${PORT}`).status(200);
 });
@@ -107,4 +43,4 @@ const onDisconnect = (socket) => () => {
 setInterval(() => {
   let gamestate = { date: new Date(), playerstates: playerstates };
   io.sockets.emit("tick", gamestate);
-}, 1000);
+}, 1000 / 60);
